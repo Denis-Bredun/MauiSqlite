73b9cd7116ccbfbd63314f2203068d9fe428d056
denis.bredun <bredun.denis@gmail.com>
2025-01-08 19:13:28 +0200

    Finished Main Page

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,32 +2,65 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        private readonly LocalDbService _localDbService;
+        private int _editCustomerId;
 
-        public MainPage()
+        public MainPage(LocalDbService localDbService)
         {
             InitializeComponent();
+            _localDbService = localDbService;
+            Task.Run(async () => listView.ItemsSource = await _localDbService.GetCustomers());
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void saveButton_Clicked(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
+            if(_editCustomerId == 0)
+            {
+                await _localDbService.Create(new Customer
+                {
+                    CustomerName = nameEntryField.Text,
+                    Email = emailEntryField.Text,
+                    Mobile = mobileEntryField.Text
+                });
+            }
             else
-                CounterBtn.Text = $"Clicked {count} times";
+            {
+                await _localDbService.Update(new Customer
+                {
+                    Id = _editCustomerId,
+                    CustomerName = nameEntryField.Text,
+                    Email = emailEntryField.Text,
+                    Mobile = mobileEntryField.Text
+                });
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+                _editCustomerId = 0;
+            }
+
+            nameEntryField.Text = string.Empty;
+            emailEntryField.Text = string.Empty;
+            mobileEntryField.Text = string.Empty;
+
+            listView.ItemsSource = await _localDbService.GetCustomers();
         }
 
-        private void saveButton_Clicked(object sender, EventArgs e)
+        private async void listView_ItemTapped(object sender, ItemTappedEventArgs e)
         {
+            var customer = (Customer)e.Item;
+            var action = await DisplayActionSheet("Action", "Cancel", null, "Edit", "Delete");
 
-        }
-
-        private void listView_ItemTapped(object sender, ItemTappedEventArgs e)
-        {
+            switch(action)
+            {
+                case "Edit":
+                    _editCustomerId = customer.Id;
+                    nameEntryField.Text = customer.CustomerName;
+                    mobileEntryField.Text = customer.Mobile;
+                    emailEntryField.Text = customer.Email;
+                    break;
+                case "Delete":
+                    await _localDbService.Delete(customer);
+                    listView.ItemsSource = await _localDbService.GetCustomers();
+                    break;
+            }
 
         }
     }
