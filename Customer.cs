c1e084ba6ad0455c060ffdefcef64a267e8236ec
denis.bredun <bredun.denis@gmail.com>
2025-01-08 18:52:12 +0200

    Added LocalDbService and Customer model

---
+++ Customer.cs
@@ -0,0 +1,27 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiSqlite
+{
+    [Table("customer")]
+    public class Customer
+    {
+        [PrimaryKey]
+        [AutoIncrement]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("cust_name")]
+        public string CustomerName { get; set; }
+
+        [Column("cust_mobile")]
+        public string Mobile { get; set; }
+
+        [Column("cust_email")]
+        public string Email { get; set; }
+    }
+}
